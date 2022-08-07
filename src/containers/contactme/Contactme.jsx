873cee3a9a4d80576d9cadc01a42c727d505b30c
BaroszHere <bartosz.kaminski1807@gmail.com>
2022-08-07 13:59:14 +0200

    v3

--- src/containers/contactme/Contactme.jsx
+++ src/containers/contactme/Contactme.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./contactme.css";
-import Blob1 from '../../assets/Blob1.png'
+import Blob1 from '../../assets/Blob1.png';
+import emailjs from 'emailjs-com'
 
 const Contactme = () => {
+  function sendEmail(e){
+    e.preventDefault();
+
+    emailjs.sendForm(
+    'service_piuivf5',
+    'template_pwjocud',
+     e.target,
+    "tkmg8HJzR_Aks0jHK").then(res=>{
+      console.log(res);
+    }).catch(err=> console.log(err));
+    e.target.reset()
+  }
   return (
   <div className="contactme section__padding" id="contactme">
     <div className="contactme__heading">
@@ -10,16 +23,16 @@ const Contactme = () => {
 </div>
 <div className="contactme__content">
 
-    <form>
+    <form onSubmit={sendEmail}>
       <p>Name</p>
-      <input type="text" id="name" placeholder="John" required/>
+      <input type="text" name="name" placeholder="John" required/>
       <p>Email</p>
-      <input type="email" id="email" placeholder="John@gmail.com" required/>
+      <input type="email" name="email" placeholder="John@gmail.com" required/>
       <p>Phone</p>
-      <input type="text" id="phone" placeholder="665 123 123" required/>
+      <input type="text" name="phone" placeholder="665 123 123" required/>
       <p>Message</p>
-      <textarea  id="message"  rows="4" placeholder="How can we help you?"></textarea>
-      <button type="submit">Send</button>
+      <textarea  name="message"  rows="4" placeholder="How can we help you?"></textarea>
+      <button type="submit" value="Send">Send</button>
     </form>
     <div className="contactme__image">
     <img src={Blob1} alt="Blob1" />
